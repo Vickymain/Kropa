db7d2612ac9b569bf1321ccd45347aab01bfc599
jomboi8 <leonardjomboi8@gmail.com>
2026-03-18 12:01:05 +0300

    chore : kropa_project and templates

---
+++ kropa_project/urls.py
@@ -0,0 +1,10 @@
+"""
+KROPA URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('diagnosis.urls')),
+]
